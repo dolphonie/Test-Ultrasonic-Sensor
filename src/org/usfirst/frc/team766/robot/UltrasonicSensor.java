d414af728807ab75b2182da875f8cf9cf57223d1
Patrick Kao <patrick.d.kao@gmail.com>
2015-01-23 20:57:03 -0800

    Prototype Code
    I wrote prototype code that *should* work but probably doesn't

--- src/org/usfirst/frc/team766/robot/UltrasonicSensor.java
+++ src/org/usfirst/frc/team766/robot/UltrasonicSensor.java
@@ -1,13 +1,6 @@
 package org.usfirst.frc.team766.robot;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-
 import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.Timer;
 
 public class UltrasonicSensor implements Runnable {
 	
